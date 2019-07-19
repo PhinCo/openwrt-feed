116ad7443ea1d07f7925d109f58ac93f2196fdc4
Scott <scott@phin.co>
2019-07-18 20:04:38 -0700

    build-feed: 1.3.8 with app_version=1.0.9,app_hash=578b239,bluegecko_version=2.0.2,bluegecko_hash=15ecc1d

--- uci2dat/files/lib/wifi/ralink_common.sh
+++ uci2dat/files/lib/wifi/ralink_common.sh
@@ -223,7 +223,7 @@ enable_ralink_wifi() {
     config_get vifs "$device" vifs
 
 	# must start up ra0 first, to create all wireless related interface
-	ifconfig ra0 up
+	#ifconfig ra0 up
 
     # bring up vifs
     for vif in $vifs; do
